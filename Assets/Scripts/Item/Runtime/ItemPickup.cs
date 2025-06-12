2fe53aca26f48b932d986f4874b5575f93007b6c
Choryu0731 <nabiiydg@naver.com>
2025-06-13 03:35:32 +0900

    feat : 아이템 Runtime, Inventory 프로토타입 추가 #1
    아이템 Runtime, Inventory 프로토타입 추가

---
+++ Assets/Scripts/Item/Runtime/ItemPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public ItemBaseData itemData;
+    public int quantity = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            var inventory = other.GetComponent<Inventory>();
+            inventory.AddItem(itemData, quantity);
+            Destroy(gameObject);
+        }
+    }
+}
