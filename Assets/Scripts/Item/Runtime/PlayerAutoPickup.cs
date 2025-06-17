d91b9cff43a12ace2ae5cc62ea29c3ed455d168e
Choryu0731 <nabiiydg@naver.com>
2025-06-17 23:52:30 +0900

    feat : 아이템 프리팹 추가, 크래프팅UI 수정 #1
    아이템 프리팹 추가, 크래프팅UI 수정

---
+++ Assets/Scripts/Item/Runtime/PlayerAutoPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAutoPickup : MonoBehaviour
+{
+    public float pickupRange = 2f;
+    public LayerMask itemLayer;
+
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, pickupRange, itemLayer);
+
+        foreach (Collider hit in hits)
+        {
+            var item = hit.GetComponent<ItemPickup>();
+            if (item != null)
+            {
+                item.PickUp(gameObject);
+            }
+        }
+    }
+}
