309a8f9e9686ce861c70af77a056de47329bb765
wnstlrWkd123 <prkjnsk@naver.com>
2025-06-18 16:36:37 +0900

    docs: 약간의 문제수정 #3
    약간의 문제수정

--- Assets/Scripts/Building/ShipInitializer.cs
+++ Assets/Scripts/Building/ShipInitializer.cs
@@ -12,8 +12,8 @@ public class ShipInitializer : MonoBehaviour
 
     void GenerateInitialTiles()
     {
-        Vector3 position = new Vector3(0, -1, 0);
-        //Vector3 position = new Vector3(-400, -1, 355);
+        //Vector3 position = new Vector3(0, -1, 0);
+        Vector3 position = new Vector3(-400, -1, 355);
         GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, shipTransform);
 
         Tile tileScript = tile.GetComponent<Tile>();
